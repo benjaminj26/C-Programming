c4edbc81fbaed7371c2fbcec2d3c89f068c1a398
Reenphy George <reenphygeorge@gmail.com>
2021-06-15 12:55:59 +0530

    Conditional Operators Added

---
+++ Operators/Conditional/greatest_of_2_nos.c
@@ -0,0 +1,13 @@
+// Program to find largest of two numbers using conditional operator //
+
+#include <stdio.h>
+int main()
+{
+    int n1, n2, g;
+    printf("Enter the first no: ");
+    scanf("%d", &n1);
+    printf("Enter the second no: ");
+    scanf("%d", &n2);
+    g = (n1 > n2) ? n1 : n2;
+    printf("Greatest no is: %d", g);
+}
