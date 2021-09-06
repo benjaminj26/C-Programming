58a653fbea5173fe06c659b0bbc3bb73b5511855
Reenphy George <reenphygeorge@gmail.com>
2021-09-06 14:22:43 +0530

    lab qn_11 added

---
+++ Lab/qn_11.c
@@ -0,0 +1,39 @@
+/*****************************************************************
+
+CREATOR - REENPHY GEORGE
+
+TITLE - Concatenate two strings without using library functions.
+
+DATE - 6th SEPTEMBER, 2021
+
+*****************************************************************/
+
+#include<stdio.h>
+
+int main()
+{
+    char str1[30] , str2[30] , str3[60];
+    
+    // Input Strings //
+    printf("Enter the 1st string: ");
+    gets(str1);
+    printf("Enter the 2nd string: ");
+    gets(str2);
+
+    // Concatenating Strings //
+    int i=0;
+    while(str1[i] != '\0')
+    {
+        str3[i] = str1[i];
+        i++;
+    }
+    int j=0;
+    while(str2[j] != '\0')
+    {
+        str3[i] = str2[j];
+        i++;
+        j++;
+    }
+    printf("Concatenated String: %s", str3);
+    return 0;
+}
